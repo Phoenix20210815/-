66008a498e7bc0d24e3999f461c0fad8a31c3151
Phoenix <88956603+Phoenix20210815@users.noreply.github.com>
2021-08-16 14:17:35 +0800

    Add files via upload
    while和for

---
+++ 08163.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+//{
+//    int i=0;
+//    while(i<=10){
+//        printf("%d",i);
+//        if(5==i)
+//        continue;
+//        i++;
+//    }
+{
+    for(int i=1;i<=10;i++)
+        {
+            if(5==i)
+            continue;
+        printf("%d",i);
+        }
+    return 0;
+}
